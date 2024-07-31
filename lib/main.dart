e8812295f7f0799e2c01f435689c398ee99450ac
unknown <ibrahimkhan.dev405@gmail.com>
2024-07-31 11:11:44 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:ecommerce_mobile_app/screens/nav_bar_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.mulishTextTheme()
+      ),
+      home: NavBarScreen(),
+    );
+  }
+}
