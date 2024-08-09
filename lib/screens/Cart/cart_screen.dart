aecc1fa5039bcd74afe68b04606fda6051dcf386
unknown <ibrahimkhan.dev405@gmail.com>
2024-08-09 16:56:53 +0600

    cart provider and cart screen crate done

--- lib/screens/Cart/cart_screen.dart
+++ lib/screens/Cart/cart_screen.dart
@@ -5,6 +5,19 @@ class CartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
